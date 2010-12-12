44d344bdc55bf6959284f44f55d7a39e64ebd382
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2010-12-12 22:15:00 +0100

    sample plugin skeleton

---
+++ inc/plugins/hello_pl.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of PluginLibrary for MyBB.
+ * Copyright (C) 2010 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+    die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+function hello_pl_info()
+{
+}
+
+function hello_pl_is_installed()
+{
+}
+
+function hello_pl_install()
+{
+}
+
+function hello_pl_uninstall()
+{
+}
+
+function hello_pl_activate()
+{
+}
+
+function hello_pl_deactivate()
+{
+}
+
+?>
